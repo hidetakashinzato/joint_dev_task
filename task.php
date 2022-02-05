29db2e90ed07a2521f443231924158500462921e
hidetakashinzato <hidetakashinzato@gmail.com>
2022-02-05 15:32:25 +0900

    Q17回答

--- task.php
+++ task.php
@@ -212,13 +212,26 @@ class User
 {
 
     // コードを追加
+private $nmae;
+private $age;
+private $gender;
 
+function __construct($user_name, $user_age, $user_gender){
+    $this-> name = $user_name;
+    $this-> age = $user_age;
+    $this-> gender = $user_gender;
+}
+function info(){
+    print("名前:".$this-> name .PHP_EOL);
+    print("年齢;".$this-> age .PHP_EOL);
+    print("性別:".$this-> gender .PHP_EOL);
+}
 }
 
 $user1 = new User("神里", 32, "男");
 $user2 = new User("あじー", 32, "男");
 
-/*$user1->info();
+$user1->info();
 print("-------------".PHP_EOL);
 $user2->info();
 
@@ -280,6 +293,6 @@ $humans = [$human1, $human2, $human3, $human4];
 
 foreach ($humans as $human) {
     $zoo->info_entry_fee($human);
-}*/
+}
 
 echo PHP_EOL;
