26e65bcb4bca9e81d2a3a0dea088347b7889efe1
hidetakashinzato <hidetakashinzato@gmail.com>
2022-02-05 20:27:29 +0900

    Q21回答

--- task.php
+++ task.php
@@ -339,3 +339,28 @@ foreach ($humans as $human) {
 }
 
 echo PHP_EOL;
+
+
+print("#####q21#####".PHP_EOL);
+
+for($i = 1; $i <= 30; $i++ ){
+    if($i % 105 == 0){
+        echo 'FizzBuzzHogo'.PHP_EOL;
+    }elseif($i % 35 == 0){
+        echo 'BuzzHogo'.PHP_EOL;
+    }elseif($i % 21 == 0){
+        echo 'FizzHogo'.PHP_EOL;
+    }elseif($i % 15 == 0){
+        echo 'FizzBuzz'.PHP_EOL;
+    }elseif($i % 7 == 0){
+        echo 'Hogo'.PHP_EOL;
+    }elseif($i % 5 == 0){
+        echo 'BuzzHogo'.PHP_EOL;
+    }elseif($i % 3 == 0){
+        echo 'Fizz'.PHP_EOL;
+    }else{
+        echo $i.PHP_EOL;
+    }
+
+}
+
